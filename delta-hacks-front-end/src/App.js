91a4fa303a39c1d580747baa2a0e90b73b2276cc
Geon Youn <geon.youn@outlook.com>
2023-01-14 21:06:23 -0500

    Switch all files to use tab indent by 4 spaces

--- delta-hacks-front-end/src/App.js
+++ delta-hacks-front-end/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import Header from "./sections/Header.js"
-import Footer from "./sections/Footer.js"
+import Header from "./sections/Header.js";
+import Footer from "./sections/Footer.js";
+import QueryItem from "./elements/QueryItem.js";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div className="middle">
-        <p>aaa</p>
-      </div>
-      <Footer />
-    </>
-  );
+    // Make users be able to search schools by 
+
+    return (
+        <>
+            <Header />
+            <div className="middle">
+                <div>
+                    <QueryItem />
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
