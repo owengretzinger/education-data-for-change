a29e059d73ff78fc48db0b6eb0b1d2fe6eb324ec
Geon Youn <geon.youn@outlook.com>
2023-01-14 21:47:18 -0500

    Add quote

--- delta-hacks-front-end/src/App.js
+++ delta-hacks-front-end/src/App.js
@@ -7,14 +7,21 @@ function App() {
     // Make users be able to search schools by 
     const onSubmitQuery = (query) => {
         console.log(`SELECT * FROM ____ WHERE school LIKE "\%${query}\%"`);
-    }; 
+    };
 
     return (
         <>
             <Header />
             <div className="middle">
+                <div className="quote">
+                    <p className="text">"Ontario is committed to the success and well-being of every student and child.
+                        Learners in the province's education system will develop the knowledge, skills
+                        and characteristics that will lead them to become personally successful, economically
+                        productive and actively engaged citizens." </p>
+                    <p className="author">- <a href="http://ncee.org/wp-content/uploads/2017/01/Ont-non-AV-10-Ontario-Government-Achieving-Excellence-A-renewed-vision-for-education-in-Ontario.pdf">Achieving Excellence: A Renewed Vision for Education in Ontario</a></p>
+                </div>
                 <div className="school-name">
-                    School name: <QueryItem onSubmitQuery={onSubmitQuery} buttonName="Go!"/>
+                    School name: <QueryItem onSubmitQuery={onSubmitQuery} buttonName="Go!" />
                 </div>
             </div>
             <Footer />
