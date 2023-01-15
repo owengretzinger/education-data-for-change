41b3798f61548a5af74c012ac98a775a3381f1ed
Geon Youn <geon.youn@outlook.com>
2023-01-14 21:33:18 -0500

    Update header to new GitHub link

--- delta-hacks-front-end/src/sections/Header.js
+++ delta-hacks-front-end/src/sections/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="header">
             <h1>{projectName}</h1>
             <ul>
-                <li><a href="https://github.com/owengretzinger/DeltaHacks" target="_blank">GitHub</a></li>
+                <li><a href="https://github.com/owengretzinger/education-data-for-change" target="_blank">GitHub</a></li>
                 <li><a href="https://deltahacks9.devpost.com/" target="_blank">DevPost</a></li>
                 <li><a href="https://deltahacks.com/" target="_blank">DeltaHacksIX</a></li>
             </ul>
